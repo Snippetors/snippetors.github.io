777de3e27e546dfb3f212653efcd3af091655a2f
Lucien Zhang <lucienzhangzl@outlook.com>
2023-01-17 16:16:41 +0900

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,28 @@
-module.exports = {
+import { defaultTheme } from '@vuepress/theme-default'
+import tabsPlugin from '@snippetors/vuepress-plugin-tabs'
+import codeCopyPlugin from '@snippetors/vuepress-plugin-code-copy'
+
+export default {
   lang: "en-US",
   title: "Snippetors' Packages",
   head: [["link", { rel: "icon", href: "/images/hero.svg" }]],
   plugins: [
-    ["@snippetors/vuepress-plugin-tabs", {}],
-    ["@snippetors/vuepress-plugin-code-copy", {}],
+    tabsPlugin({}),
+    codeCopyPlugin({}),
   ],
-  themeConfig: {
-    logo: "/images/hero.svg",
-    repo: "Snippetors/snippetors.github.io",
-    docsDir: "docs",
-    navbar: [
-      { text: "vuepress-plugin-tabs", link: "/plugins/vuepress-plugin-tabs" },
-      {
-        text: "vuepress-plugin-code-copy",
-        link: "/plugins/vuepress-plugin-code-copy",
-      },
-    ],
-    sidebar: false,
-  },
+  theme: defaultTheme(
+    {
+      logo: "/images/hero.svg",
+      repo: "Snippetors/snippetors.github.io",
+      docsDir: "docs",
+      navbar: [
+        { text: "vuepress-plugin-tabs", link: "/plugins/vuepress-plugin-tabs" },
+        {
+          text: "vuepress-plugin-code-copy",
+          link: "/plugins/vuepress-plugin-code-copy",
+        },
+      ],
+      sidebar: false,
+    },
+  ),
 };
